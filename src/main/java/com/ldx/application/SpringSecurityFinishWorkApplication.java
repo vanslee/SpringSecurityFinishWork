7d3c5c1e0c09e711d7be387b32c446cf26dde712
李登祥 <vanslee62@gmail.com>
2022-06-20 12:51:21 +0800

    <create> 项目创建

---
+++ src/main/java/com/ldx/application/SpringSecurityFinishWorkApplication.java
@@ -0,0 +1,13 @@
+package com.ldx.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFinishWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityFinishWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ldx/application/SpringSecurityFinishWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldx.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFinishWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
